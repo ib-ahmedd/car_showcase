daec28b9c2bd9955d04c334f4656a01c040ae2d9
Ibrahim Ahmed <ahmed1768476@gmail.com>
2024-05-14 21:40:09 +0100

    complete non functional hero, navbar, and footer sections

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./globals.css";
+import { Footer, Navbar } from "@components";
+
+export const metadata = {
+  title: "Car Hub",
+  description: "Discover the best cars in the world",
+};
 
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 };
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
+  const handleScroll = () => {};
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -10,8 +13,21 @@ const Hero = () => {
 
         <p className="hero__subtitle">
           Steamline your car rental experience with our effortless booking
-          process
+          process.
         </p>
+
+        <CustomButton
+          title={"Explore Cars"}
+          containerStyles={"bg-primary-blue text-white rounded-full mt-10"}
+          handleClick={handleScroll}
+        />
+      </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
